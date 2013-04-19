5ed2a4b53258146643d7327f91eab4bb22591faa
Kellie Canida <kelliecanida@hotmail.com>
2013-04-19 00:49:53 -1000

    fix model test for changes made in constructor method of entites

--- test/ModelTest.java
+++ test/ModelTest.java
@@ -40,19 +40,19 @@ public class ModelTest {
   public void testModel() {
     //Create 1 tag that's associated with 1 product.
     Tag tag = new Tag("Tag");
-    Product product = new Product("Product", "Description");
-    product.tags.add(tag);
-    tag.products.add(product);
+    Product product = new Product("1", "Product", "Description");
+    product.getTags().add(tag);
+    tag.getProducts().add(product);
     
     //Create 1 warehouse that's associated with 1 StockItem for 1 Product.
-    Warehouse warehouse = new Warehouse("Warehouse");
-    StockItem stockitem = new StockItem(warehouse, product, 100);
-    warehouse.stockitems.add(stockitem);
+    Warehouse warehouse = new Warehouse("1", "Warehouse");
+    StockItem stockitem = new StockItem("1", warehouse, product, 100);
+    warehouse.getStockItems().add(stockitem);
     //stockitem.warehouse = warehouse;  REDUNDANT STATEMENT IS UNNECESSARY
     
     //Create 1 address that's associated with 1 warehouse
-    Address address = new Address("Address", warehouse);
-    warehouse.address = address;
+    Address address = new Address("1", "street","city","state","zip",warehouse);
+    warehouse.setAddress(address);
     
     
     //Persist the sample model by saving all entities and relationships.
@@ -77,26 +77,26 @@ public class ModelTest {
     assertEquals("Checking address", addresses.size(), 1);
     
     //Check that we've recovered all relationships
-    assertEquals("Warehouse-StockItem", warehouses.get(0).stockitems.get(0), stockitems.get(0));
-    assertEquals("StockItem-Warehouse", stockitems.get(0).warehouse, warehouses.get(0));
-    assertEquals("Product-StockItem", products.get(0).stockitems.get(0), stockitems.get(0));
-    assertEquals("StockItem-Product", stockitems.get(0).product, products.get(0));
-    assertEquals("Product-Tag", products.get(0).tags.get(0), tags.get(0));
-    assertEquals("Tag-Product", tags.get(0).products.get(0), products.get(0));
-    assertEquals("Warehouse-Address", warehouses.get(0).address, addresses.get(0));
-    assertEquals("Address-Warehouse", addresses.get(0).warehouse, warehouses.get(0));
+    assertEquals("Warehouse-StockItem", warehouses.get(0).getStockItems().get(0), stockitems.get(0));
+    assertEquals("StockItem-Warehouse", stockitems.get(0).getWarehouse(), warehouses.get(0));
+    assertEquals("Product-StockItem", products.get(0).getStockItems().get(0), stockitems.get(0));
+    assertEquals("StockItem-Product", stockitems.get(0).getProduct(), products.get(0));
+    assertEquals("Product-Tag", products.get(0).getTags().get(0), tags.get(0));
+    assertEquals("Tag-Product", tags.get(0).getProducts().get(0), products.get(0));
+    assertEquals("Warehouse-Address", warehouses.get(0).getAddress(), addresses.get(0));
+    assertEquals("Address-Warehouse", addresses.get(0).getWarehouse(), warehouses.get(0));
     
     //Some code to illustrate model manipulation with ORM
     //Start in Java. Delete the tag from the (original) product instance.
-    product.tags.clear();
+    product.getTags().clear();
     //Persist the revised product instance.
     product.save();
     //FYI: this does not change our previously retrieved instance from the database.
-    assertTrue("Previously retrieved product still has tag", !products.get(0).tags.isEmpty());
+    assertTrue("Previously retrieved product still has tag", !products.get(0).getTags().isEmpty());
     //But of course it does change a freshly retrieved product instance.
-    assertTrue("Fresh Product has not tag", Product.find().findList().get(0).tags.isEmpty());
+    assertTrue("Fresh Product has not tag", Product.find().findList().get(0).getTags().isEmpty());
     //Note: Freshly retrieved Tag does not point to any Product.
-    assertTrue("Fresh Tag has no Products", Tag.find().findList().get(0).products.isEmpty());
+    assertTrue("Fresh Tag has no Products", Tag.find().findList().get(0).getProducts().isEmpty());
     //We can now delete this Tag from the database if we want.
     tag.delete();
     assertTrue("No more tags in database", Tag.find().findList().isEmpty());
